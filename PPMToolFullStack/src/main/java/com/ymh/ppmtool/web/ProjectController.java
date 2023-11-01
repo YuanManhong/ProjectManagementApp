cad07ccc0964f10fd1052a948de4ef04a5e2dd58
Manhong Yuan <Yuan.Manh@northeastern.edu>
2023-11-01 18:53:45 -0400

    added BindingResult and modified response type to generic to customize response for invalid request

--- PPMToolFullStack/src/main/java/com/ymh/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/ymh/ppmtool/web/ProjectController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,11 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@Valid @RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project,
+                                                    BindingResult result){
+        if (result.hasErrors()){
+            return new ResponseEntity<String>("Invalid object", HttpStatus.BAD_REQUEST);
+        }
         Project newProject = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(newProject, HttpStatus.CREATED);
     }
